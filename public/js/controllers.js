a646ee97362b5e25044574f0793690cf51eb0968
mollywhitnack <mollywhitnack@gmail.com>
2016-07-11 12:04:26 -0700

    mixed content fix

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -47,7 +47,7 @@ app.controller('mainCtrl', function($scope, $state, $auth, $rootScope, $http, $q
     $scope.showSelect = true;
     //console.log("in get stock: " , $scope.stock.name);
     console.log("in get stock: " , name);
-    var url = `http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=${name}&callback=JSON_CALLBACK`
+    var url = `//dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=${name}&callback=JSON_CALLBACK`
     console.log("url:" , url);
     $http.jsonp(url)
     .success(function(data) {
@@ -118,7 +118,7 @@ app.controller('profileCtrl', function($scope, Profile, ProfileByID, $state, Use
   for(var i =0; i< $rootScope.currentUser.stocks.length; i++){
       //console.log("in get stock: " , $scope.stock.name);
       let sym = $rootScope.currentUser.stocks[i];
-      var url = `http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=${sym}&callback=JSON_CALLBACK`
+      var url = `//dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=${sym}&callback=JSON_CALLBACK`
       $http.jsonp(url)
       .success(function(data) {
           console.log("data: ", data);
